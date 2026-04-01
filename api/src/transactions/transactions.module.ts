26c3293e599053f9aaf1c2bb01fd07f3eef08fe1
Gerrard-Lewu <gerrardlewu@gmail.com>
2026-04-01 02:15:38 +0200

    modified transaction/ files by fixing errors and updating dto & controller

--- api/src/transactions/transactions.module.ts
+++ api/src/transactions/transactions.module.ts
@@ -3,10 +3,14 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
 import { Transaction } from './entities/transaction.entity';
+import { AwsModule } from '../aws/aws.module';
 
 @Module({
   // Import the TypeORM feature to recognize the Transaction entity
-  imports: [TypeOrmModule.forFeature([Transaction])],
+  imports: [
+    TypeOrmModule.forFeature([Transaction]), 
+    AwsModule // Add to the imports array
+  ],
   controllers: [TransactionsController],
   providers: [TransactionsService],
 })
--- api/src/transactions/transactions.service.ts
+++ api/src/transactions/transactions.service.ts
@@ -1,26 +1,55 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Logger } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateTransactionDto } from './dto/create-transaction.dto';
 import { UpdateTransactionDto } from './dto/update-transaction.dto';
+import { Transaction } from './entities/transaction.entity';
+import { AwsService } from '../aws/aws.service';
 
 @Injectable()
 export class TransactionsService {
-  create(createTransactionDto: CreateTransactionDto) {
-    return 'This action adds a new transaction';
+  private readonly logger = new Logger(TransactionsService.name);
+
+  constructor(
+    @InjectRepository(Transaction)
+    private transactionRepository: Repository<Transaction>,
+    private awsService: AwsService, // <-- Inject your AWS Service
+  ) {}
+
+  async create(createTransactionDto: CreateTransactionDto) {
+    // 1. Save the structured data to PostgreSQL
+    const newTransaction = this.transactionRepository.create(createTransactionDto);
+    const savedTransaction = await this.transactionRepository.save(newTransaction);
+    this.logger.log(`Saved transaction ${savedTransaction.id} to Postgres`);
+
+    // 2. Fire an event to AWS SQS for the DR/Audit pipeline
+    try {
+      await this.awsService.sendTransactionMessage({
+        eventType: 'TRANSACTION_CREATED',
+        source: 'CORE_API',
+        data: savedTransaction,
+        timestamp: new Date().toISOString(),
+      });
+    } catch (error) {
+      this.logger.error(`Failed to send transaction ${savedTransaction.id} to SQS`, error.stack);
+    }
+
+    return savedTransaction;
   }
 
   findAll() {
-    return `This action returns all transactions`;
+    return this.transactionRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} transaction`;
+  findOne(id: string) {
+    return this.transactionRepository.findOneBy({ id });
   }
 
-  update(id: number, updateTransactionDto: UpdateTransactionDto) {
+  update(id: string, updateTransactionDto: UpdateTransactionDto) {
     return `This action updates a #${id} transaction`;
   }
 
-  remove(id: number) {
+  remove(id: string) {
     return `This action removes a #${id} transaction`;
   }
 }
